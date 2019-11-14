1635009f04ab50ed35b27af40f3216d60b45926c
phltc <phltc@G3C36.windows.cin.ufpe.br>
2019-11-14 15:46:00 -0300

    Minha parte do projeto :)

---
+++ ProjetoIP/src/entregador/CadastroEntregador.java
@@ -0,0 +1,35 @@
+package entregador;
+
+public class CadastroEntregador {
+
+	private RepositorioEntregadores repositorio;
+
+	public CadastroEntregador(RepositorioEntregadores repo) {
+		repositorio = repo;
+	}
+
+	public void cadastrarEntregador(Entregador entregador) throws EntregadorJaCadastroException {
+		if (!this.repositorio.verificarEntregador(entregador.getCpf())) {
+			this.repositorio.inserirEntregador(entregador);
+		} else {
+			throw new EntregadorJaCadastroException();
+		}
+	}
+
+	public void removerEntregador(String cpf) throws EntregadorNaoEncontradoException {
+		this.repositorio.removerEntregador(cpf);
+	}
+
+	public boolean verificarEntregador(String cpf) {
+		return this.repositorio.verificarEntregador(cpf);
+	}
+
+	public void atualizarTransporteEntregador(String cpf, String transporte) throws EntregadorNaoEncontradoException {
+		this.repositorio.atualizarTransporteEntregador(cpf, transporte);
+	}
+
+	public Entregador procurarEntregador(String cpf) throws EntregadorNaoEncontradoException {
+		return this.repositorio.procurarEntregador(cpf);
+	}
+
+}
